72d6704ef21b3034a160518615bcbb8af80c1c8a
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-10 02:23:59 +0300

    Migrations commands was applied

--- EFCore.CodeFirst/DAL/Product.cs
+++ EFCore.CodeFirst/DAL/Product.cs
@@ -12,5 +12,6 @@ namespace EFCore.CodeFirst.DAL
         public string Name { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
+        public string Barcode { get; set; }
     }
 }
