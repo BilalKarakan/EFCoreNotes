de1e7202c2d3b87b16b1b56a38e8086c9ccef1ac
Bilal Karakan <bilalkaraka36@gmail.com>
2023-09-12 11:37:58 +0300

    Remove and Script migrations

--- EFCore.CodeFirst/DAL/Product.cs
+++ EFCore.CodeFirst/DAL/Product.cs
@@ -11,7 +11,7 @@ namespace EFCore.CodeFirst.DAL
         public int Id { get; set; }
         public string Name { get; set; }
         public int Stock { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public string Barcode { get; set; }
     }
 }
